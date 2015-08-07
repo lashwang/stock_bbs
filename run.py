bf393d30c3623fd2894a8bebf1df9cd1a41ce748
Simon <lashwang@sina.com>
2015-08-07 09:37:52 +0800

    add command line to run different scrapy

--- run.py
+++ run.py
@@ -3,7 +3,17 @@
 __author__ = 'Simon'
 
 from scrapy import cmdline
+import sys
+
+
+def main():
+    print sys.argv
+    if len(sys.argv) == 2:
+        cmdline.execute(("scrapy crawl " + sys.argv[1]).split())
 
 if __name__ == '__main__':
-    cmdline.execute("scrapy crawl stock_list".split())
-    #cmdline.execute("scrapy crawl stock_detail".split())
+    main()
+
+
+
+
